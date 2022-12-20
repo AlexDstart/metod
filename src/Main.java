ff4b33aed20d5981c524b2d4f8f090e001aef5c8
AlexWorkPC <demyanalex@gmail.com>
2022-12-21 01:13:37 +0300

    task2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
+import java.time.LocalDate;
 
 public class Main {
+    private final static int CURRENTYEAR= LocalDate.now().getYear();
     public static void main(String[] args) {
         printLeapOrNotLeap(2024);
+        //nexttask
+        printVersionOS(2014,1);
+        //nexttask
     }
 public static boolean isleapYear(int year){
         return year % 4==0 && year%100 !=0||year %400==0;
@@ -15,4 +20,26 @@ public static void printLeapOrNotLeap(int year){
 
 
 }
+public static boolean isNowYear(int year){
+        return year == CURRENTYEAR;
 }
+public static String getVersionOS(int versionOS){
+        if(versionOS==0){
+            return "IOS";
+        }else if(versionOS==1) {
+            return "Android";
+        }
+        return " Мы не знаем такой операционной системы ";
+}
+public static void printVersionOS(int year,int versionOS){
+        if (isNowYear(year)){
+            System.out.println(" Установите полную версию для вашего "+ getVersionOS(versionOS));
+        }else {
+            System.out.println(" Установите облегченную версию для вашего "+ getVersionOS(versionOS));
+        }
+
+}
+}
+
+
+
